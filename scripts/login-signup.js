141320a3f81637228c551c220323afacaaaf3f9a
kanavsingla28 <kanavsingla28@gmail.com>
2021-08-27 11:26:48 +0530

    Login save user object in db

--- scripts/login-signup.js
+++ scripts/login-signup.js
@@ -7,14 +7,15 @@ function signUp(){
     console.log("Testing scripting working or not")
     let allUsers = JSON.parse(localStorage.getItem("allUsers"));
     if(allUsers==null) allUsers=[];
-    allUsers.push({
+    var newUsertoAdd = {
         id: uniqueUserId,
         userFName: document.querySelector("#fName").value,
         userLName: document.querySelector("#lName").value,
         userEmail: document.querySelector("#email").value,
         userPass: document.querySelector("#password").value
-    });
-    localStorage.setItem("loggedDetails",JSON.stringify([{isLogged:true,userLogged:uniqueUserId}]))
+    };
+    allUsers.push(newUsertoAdd);
+    localStorage.setItem("loggedDetails",JSON.stringify([{isLogged:true},newUsertoAdd]))
     localStorage.setItem("uniqueUserCounter",JSON.stringify(++uniqueUserId));
     localStorage.setItem("allUsers",JSON.stringify(allUsers));
 
@@ -33,7 +34,7 @@ function logIn(){
     let mailEntered = document.querySelector("#email").value;
     let passEntered = document.querySelector("#password").value;
     if(checkUserDetails(mailEntered,passEntered)){
-        localStorage.setItem("loggedDetails",JSON.stringify([{isLogged:true,userLogged:userIdforLogin}]));
+        localStorage.setItem("loggedDetails",JSON.stringify([{isLogged:true},userIdforLogin]));
         document.querySelector("#login-btn-id").textContent = "Login Success...";
         setTimeout(()=>{
             location.href = "./../index.html";
@@ -49,7 +50,7 @@ function checkUserDetails(mailEntered,passEntered){
     let allUsers = JSON.parse(localStorage.getItem("allUsers"));
     for(var i=0; i<allUsers.length; i++){
         if(allUsers[i].userEmail==mailEntered && allUsers[i].userPass==passEntered){
-            userIdforLogin = allUsers[i].id;
+            userIdforLogin = allUsers[i];
             return true;
         }
     }
