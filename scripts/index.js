1dcde7a455ea8295c4b824fcefeb68f6bfb874f2
Abhinav <abhinavgujral33@gmail.com>
2021-08-28 20:45:47 +0530

    improved home page and investment page

--- scripts/index.js
+++ scripts/index.js
@@ -9,15 +9,19 @@ function signout(){
     localStorage.setItem("loggedDetails",JSON.stringify(user_data));
 
     /* ---remove dropdown--- */
-    var get_dropdown=document.getElementsByClassName("index_nav_drop");
+    var get_dropdown=document.querySelector(".index_nav_drop");
+    location.href="./index.html";
     get_dropdown.style.display= 'none';
-    
+
          location.href="/index.html";
 
 }
 
 function check_if_logedin_navbar(){
     /*--------get-status------ */
+    console.log(localStorage.getItem("loggedDetails"));
+
+ //   if(thisSession.hasOwnProperty('loggedDetails')){
    var user_data= JSON.parse(localStorage.getItem("loggedDetails"));
    var status= user_data[0].isLogged;
 
@@ -28,8 +32,8 @@ function check_if_logedin_navbar(){
        /* remove anchor tag SignIn and add name*/
 let nav_anchor_Signin=document.getElementById("nav_anchor_Signin");
 
-     nav_anchor_Signin.innerText= user_data[1].userFName + " "+user_data[1].userLName;
-
+     nav_anchor_Signin.innerText=  user_data[1].userFName + " "+user_data[1].userLName+" Signed In:";
+       nav_anchor_Signin.style.color="#19955D";
           document.getElementById('drop_username').innerText=user_data[1].userFName + " "+user_data[1].userLName;
           nav_anchor_Signin.removeAttribute('href');
                   
@@ -37,28 +41,39 @@ let nav_anchor_Signin=document.getElementById("nav_anchor_Signin");
    
         }else{
 }
-
+    
 }
 function showdropdown(){
     console.log("hello11")
     var get_dropdown=document.querySelector(".index_nav_drop");
     console.log(get_dropdown.classList);
-/* 
-   if(get_dropdown.style.display.value==='none'){
+    console.log('check:1-',get_dropdown.style.display.value);
+
+  /*  if(get_dropdown.style.display.value===undefined){
+    console.log('check:2-',get_dropdown.style.display.value);
     get_dropdown.style.display='block';
-    console.log("setblock");
+   // console.log("setblock");
    }else if(get_dropdown.style.display==='block'){
-    console.log("setnone");
+   // console.log("setnone");
+    console.log('check:3-',get_dropdown.style.display.value);
     get_dropdown.style.display=='none';
    } */
     if(get_dropdown.classList.contains("classdisplay")){
+        console.log("hellloooooo:")
           get_dropdown.classList.remove('classdisplay');
         }
         else{
         get_dropdown.setAttribute('class','classdisplay');
         console.log("set none");
     } 
-   /*  get_dropdown.style.display= 'block'; 
- */
+   /*  get_dropdown.style.display= 'block';  */
+}
+
+
+function email_success_display(e){
+    
+var p=document.querySelector("#index_email_valid");
+p.innerText="Email Registered Successfully!  Thank You  😄";
+e.preventDefault();
 }
 check_if_logedin_navbar();
